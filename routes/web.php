6ea07b739f07bf85d7e3fe966fff228cc69aa98f
Yibrian <yibrian2205@gmail.com>
2025-04-03 11:03:04 -0500

    Rama principal

--- routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return response()->json(['error' => 'Inicie sesión'], Response::HTTP_UNAUTHORIZED);
-})->name('login');
